2752be2b6a3d2d3ac9a554f04bda58067bee4352
Jason Teplitz <jteplitz602@gmail.com>
2017-02-01 13:33:19 -0800

    perf: Avoid cloning log entries

--- src/server/log.rs
+++ src/server/log.rs
@@ -1,7 +1,6 @@
 /// This module abstracts away local log storage
 /// ... and eventually snapshotting.
 
-#[derive(Clone)]
 pub struct Entry {
     index: u64,
     term: u64,
@@ -37,12 +36,12 @@ impl Log for MemoryLog {
     }
 
     fn append_entries(&mut self, entries: Vec<Entry>) -> u64 {
-        self.entries.extend(entries.iter().cloned()); // TODO: can we not clone this pls
+        self.entries.extend(entries); 
         self.get_last_entry_index()
     }
 
     fn append_entry(&mut self, entry: Entry) -> u64 {
-        self.entries.push(entry.clone());
+        self.entries.push(entry);
         self.get_last_entry_index()
     }
 
