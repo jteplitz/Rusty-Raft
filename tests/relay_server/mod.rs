8dc6212069aca49fad30b7558ccd0cb187d10d4f
Jason Teplitz <jteplitz602@gmail.com>
2017-03-11 22:25:40 -0800

    test(RelayServer): Fix tcp reading bug.
    Add simple relaying test

--- tests/relay_server/mod.rs
+++ tests/relay_server/mod.rs
@@ -6,7 +6,7 @@
 //! #Examples
 //!
 //! ```
-//! let relay_server = RelayServer::new_with_addresses(NUM_ADDRESSES);
+//! let relay_server = RelayServer::new_with_random_addresses(NUM_ADDRESSES);
 //! let addrs = relay_server.get_bound_addresses();
 //! for addr in addrs {
 //!     // start up a new Raft server (can pass in all bound addrs as the addrs for the other
@@ -55,7 +55,7 @@ impl RelayServer {
     pub fn bind_random_addresses(&mut self, num_addresses: u64) {
         let mut addrs = self.addrs.lock().unwrap();
         for i in 0..num_addresses {
-            let listener = TcpListener::bind("127.0.0.0:0").unwrap();
+            let listener = TcpListener::bind("127.0.0.1:0").unwrap();
             let socket_info = SocketInfo {to_addr: None, online: false};
             addrs.insert(listener.local_addr().unwrap(), socket_info);
 
@@ -141,7 +141,7 @@ impl RelayServer {
     /// Panics if the from_stream cannot be read from or the to_stream can
     /// not be written to
     fn relay_stream_to_stream(mut from_stream: TcpStream, mut to_stream: TcpStream) {
-        let mut buf: Vec<u8> = Vec::new();
+        let mut buf = [0; 100];
 
         loop {
             let bytes_read = from_stream.read(&mut buf[..]).unwrap();
@@ -152,5 +152,67 @@ impl RelayServer {
             to_stream.write(&buf[0..bytes_read]);
             to_stream.flush();
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate rand;
+    use super::*;
+    use std::thread;
+    use std::sync::mpsc::{channel, Receiver};
+    use std::net::{SocketAddr, TcpListener, TcpStream};
+    use std::vec;
+    use std::io::{Read, Write};
+    use self::rand::{thread_rng, Rng};
+    use std::time::Duration;
+
+    // Timeout value, in ms, when waiting for local network messages
+    const TIMEOUT: u64 = 5000;
+
+    #[test]
+    /// Tests that the relay server will relay from A->B
+    fn it_relays_in_one_direction() {
+        const MESSAGE_LENGTH: usize = 1057;
+
+        let mut relay_server = RelayServer::new_with_random_addresses(1);
+        let addresses = relay_server.get_bound_addresses();
+        assert_eq!(addresses.len(), 1);
+
+        let (addr, rx) = start_tcp_server();
+        relay_server.relay_address(addresses[0], addr);
+
+        let msg: String = thread_rng().gen_ascii_chars().take(MESSAGE_LENGTH).collect();
+        {
+            let mut client = TcpStream::connect(addresses[0]).unwrap();
+            // generate a random message string
+            assert_eq!(msg.as_bytes().len(), MESSAGE_LENGTH);
+            client.write_all(msg.as_bytes()).unwrap();
+            client.flush().unwrap();
+        }
+        
+        let result = rx.recv_timeout(Duration::from_millis(TIMEOUT)).unwrap();
+        assert_eq!(msg.as_bytes(), &result[..]);
+    }
+
+    /// Starts a simple TCP server on the returned address (OS assigned) in a background thread
+    /// that sends every message it recieves down through the returned channel
+    fn start_tcp_server() -> (SocketAddr, Receiver<Vec<u8>>) {
+        let (tx, rx) = channel();
+        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        let addr = listener.local_addr().unwrap();
+        thread::spawn(move || {
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(mut s) => {
+                        let mut v = Vec::new();
+                        s.read_to_end(&mut v);
+                        tx.send(v).unwrap();
+                    },
+                    Err(e) => {}
+                }
+            }
+        });
+        (addr, rx)
     }
 }
